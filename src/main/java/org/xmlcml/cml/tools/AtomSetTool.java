74365ccb6feb3d7109eda0171e199a058400acf6
Egon Willighagen <egon.willighagen@gmail.com>
2023-12-17 14:09:41 +0100

    Does the double * matter?

--- src/main/java/org/xmlcml/cml/tools/AtomSetTool.java
+++ src/main/java/org/xmlcml/cml/tools/AtomSetTool.java
@@ -1,4 +1,5 @@
-/* Copyright 2011 Peter Murray-Rust et. al.
+/**
+ * Copyright 2011 Peter Murray-Rust et. al.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
